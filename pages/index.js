15c10babafb22647c5a947d71c5a5373e455274f
wicak <aditya.wicaksono@tanihub.com>
2022-04-10 18:58:16 +0700

    [ADD] infinite scroll on pokedex

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 import client from "../graphql/client";
 import { GET_POKEMONS } from "../graphql/queries";
 import Head from "next/head";
@@ -12,16 +12,30 @@ import {
 } from "../components/global/Global";
 import PokemonCard from "../components/global/PokemonCard";
 import MyPokemonCarousel from "../components/pokemon-list/MyPokemonCarousel";
+import Loader from "../components/global/Loader";
 import { db } from "../utils/db";
 
 export default function Home({ pokemonList }) {
+  const [pokemons, setPokemons] = useState(pokemonList);
+  const [offset, setOffset] = useState(20);
   const [myPokemons, setMyPokemons] = useState([]);
+  const fetchMorePokemons = async () => {
+    const { data } = await client.query({
+      query: GET_POKEMONS,
+      variables: {
+        limit: 20,
+        offset: offset,
+      },
+    });
+    setOffset(offset + 20);
+    setPokemons([...pokemons, ...data.pokemons.results]);
+  };
 
   useEffect(() => {
     db.myPokemons.toArray().then((pokemons) => {
       setMyPokemons(pokemons.slice(0, 5));
     });
-  })
+  }, []);
   return (
     <div>
       <Head>
@@ -41,16 +55,34 @@ export default function Home({ pokemonList }) {
       <TitleWrapper>
         <Title>Pokédex</Title>
       </TitleWrapper>
-      <TwoColumnGrid>
-        {pokemonList.map((pokemon, idx) => (
-          <PokemonCard
-            key={idx}
-            id={idx + 1}
-            image={pokemon.image}
-            name={pokemon.name}
-          />
-        ))}
-      </TwoColumnGrid>
+      <InfiniteScroll
+        dataLength={pokemons.length}
+        next={fetchMorePokemons}
+        loader={
+          <div
+            style={{
+              margin: "3rem 0",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Loader />
+          </div>
+        }
+        hasMore
+      >
+        <TwoColumnGrid>
+          {pokemons.map((pokemon, idx) => (
+            <PokemonCard
+              key={idx}
+              id={idx + 1}
+              image={pokemon.image}
+              name={pokemon.name}
+            />
+          ))}
+        </TwoColumnGrid>
+      </InfiniteScroll>
     </div>
   );
 }
