ac07b3ba8d31c07e51400032167db031a1956f9f
wicak <aditya.wicaksono@tanihub.com>
2022-04-10 20:23:59 +0700

    [FIX] limit to call next function on infinitescroll

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ export default function Home({ pokemonList }) {
             <Loader />
           </div>
         }
-        hasMore
+        hasMore={pokemons.length < 981}
       >
         <TwoColumnGrid>
           {pokemons.map((pokemon, idx) => (
